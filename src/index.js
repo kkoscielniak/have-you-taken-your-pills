0d4674ebc814cbf5ddca2928c2871032c34ba8b1
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2018-12-03 10:48:30 +0100

    Add iOS icon

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker'; // eslint-disable-line import/no-namespace
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.register();
